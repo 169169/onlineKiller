5f7776bc6cbe1e02ab71d21572a3a04a56788e77
42thcoder <42thcoder@gmail.com>
2013-04-29 16:12:28 +0800

    设置好Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineKiller::Application.config.secret_token = 'b37829a10c16a2d4bbcb1e10a6a2e2ae09b6640837cb4bc51fc69981347810969382bd12edddc49dc36c5aec4e173e0303f739bf9502b56cf75e1d3f743857b1'
